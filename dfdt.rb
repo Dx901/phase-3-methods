c0f6693c5ba45b9f1cef4df6bcb92701b98014f0
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-10-17 11:35:08 +0300

    Done

---
+++ dfdt.rb
@@ -0,0 +1,15 @@
+# def say_hi(name= "Dyes")
+#     puts "Hi there, #{name}!"
+# end
+
+# say_hi
+
+# say_hi "Sakwa"
+
+def stylish_painter
+    best_hairstyle = "Bob Ross"
+    return "Jean-Michel Basquiat"
+    best_hairstyle
+end
+  
+print (stylish_painter)
